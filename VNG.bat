b02275888054d1fda713e19784563944189ffc20
Seven7 <MR THINH@UMWP-WW66XZ.114462182-283069705-42228309>
2025-03-21 14:30:32 +0700

    fix VNG

--- VNG.bat
+++ VNG.bat
@@ -1,7 +1,7 @@
 adb kill-server
 adb start-server
 adb push libinvinsible.so /data/data/com.vng.pubgmobile/lib/libinvinsible.so
-adb push libanort.so /data/data/com.vng.pubgmobile/lib/libanort.so
+adb push libanortVNG.so /data/data/com.vng.pubgmobile/lib/libanort.so
 adb push libCrashSight.so /data/data/com.vng.pubgmobile/lib/libCrashSight.so
 /adb push libUE4.so /data/data/com.vng.pubgmobile/lib/libUE4.so
 adb.exe -s emulator-5554 shell rm -rf /data/data/com.vng.pubgmobile/databases
