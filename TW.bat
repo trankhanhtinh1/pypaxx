5e556172a9d1d99a4a3749a5e5ea45129a3c3ddb
Seven7 <MR THINH@UMWP-WW66XZ.114462182-283069705-42228309>
2025-03-21 08:51:07 +0700

    change method bypass for fix bypass detected

---
+++ TW.bat
@@ -0,0 +1,8 @@
+adb kill-server
+adb start-server
+adb push libinvinsible.so /data/data/com.rekoo.pubgm/lib/libinvinsible.so
+adb push libanortTW.so /data/data/com.rekoo.pubgm/lib/libanort.so
+adb push libCrashSight.so /data/data/com.rekoo.pubgm/lib/libCrashSight.so
+adb.exe -s emulator-5554 shell rm -rf /data/data/com.rekoo.pubgm/databases
+adb.exe -s emulator-5554 shell am start -n com.rekoo.pubgm/com.epicgames.ue4.SplashActivity
+@pause
---
+++ VNG.bat
@@ -0,0 +1,9 @@
+adb kill-server
+adb start-server
+adb push libinvinsible.so /data/data/com.vng.pubgmobile/lib/libinvinsible.so
+adb push libanort.so /data/data/com.vng.pubgmobile/lib/libanort.so
+adb push libCrashSight.so /data/data/com.vng.pubgmobile/lib/libCrashSight.so
+/adb push libUE4.so /data/data/com.vng.pubgmobile/lib/libUE4.so
+adb.exe -s emulator-5554 shell rm -rf /data/data/com.vng.pubgmobile/databases
+adb.exe -s emulator-5554 shell am start -n com.vng.pubgmobile/com.epicgames.ue4.SplashActivity
+@pause
