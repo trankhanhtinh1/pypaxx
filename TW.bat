b56a35e43b6f53929da0c0b760641d1e8374cc8b
trankhanhtinh1 <ghettaodi22342@gmail.com>
2025-03-22 14:29:17 +0700

    update bypass vip

--- TW.bat
+++ TW.bat
@@ -3,5 +3,6 @@ adb start-server
 adb push libinvinsible.so /data/data/com.rekoo.pubgm/lib/libinvinsible.so
 adb push libanortTW.so /data/data/com.rekoo.pubgm/lib/libanort.so
 adb push libCrashSight.so /data/data/com.rekoo.pubgm/lib/libCrashSight.so
+adb push libGVoicePlugin.so /data/data/com.rekoo.pubgm/lib/libGVoicePlugin.so
 adb.exe -s emulator-5554 shell rm -rf /data/data/com.rekoo.pubgm/databases
 adb.exe -s emulator-5554 shell am start -n com.rekoo.pubgm/com.epicgames.ue4.SplashActivity
