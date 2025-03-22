0748523fe17eb55d5d8e88199c9374df4a573ddb
trankhanhtinh1 <ghettaodi22342@gmail.com>
2025-03-22 18:25:42 +0700

    fix VNG

--- VNG.bat
+++ VNG.bat
@@ -3,6 +3,6 @@ adb start-server
 adb push libinvinsible.so /data/data/com.vng.pubgmobile/lib/libinvinsible.so
 adb push libanortVNG.so /data/data/com.vng.pubgmobile/lib/libanort.so
 adb push libCrashSight.so /data/data/com.vng.pubgmobile/lib/libCrashSight.so
-/adb push libUE4.so /data/data/com.vng.pubgmobile/lib/libUE4.so
+adb push libGVoicePlugin.so /data/data/com.vng.pubgmobile/lib/libGVoicePlugin.so
 adb.exe -s emulator-5554 shell rm -rf /data/data/com.vng.pubgmobile/databases
 adb.exe -s emulator-5554 shell am start -n com.vng.pubgmobile/com.epicgames.ue4.SplashActivity
