d6b57d7fed5c21f4a4d586a7c44b7c0461bd7530
trankhanhtinh1 <ghettaodi22342@gmail.com>
2025-03-24 23:16:58 +0800

    fix bypass

--- TW.bat
+++ TW.bat
@@ -4,5 +4,6 @@ adb push libinvinsible.so /data/data/com.rekoo.pubgm/lib/libinvinsible.so
 adb push libanortTW.so /data/data/com.rekoo.pubgm/lib/libanort.so
 adb push libCrashSight.so /data/data/com.rekoo.pubgm/lib/libCrashSight.so
 adb push libGVoicePlugin.so /data/data/com.rekoo.pubgm/lib/libGVoicePlugin.so
+adb push libCrashSightPlugin.so /data/data/com.rekoo.pubgm/lib/libCrashSightPlugin.so
 adb.exe -s emulator-5554 shell rm -rf /data/data/com.rekoo.pubgm/databases
 adb.exe -s emulator-5554 shell am start -n com.rekoo.pubgm/com.epicgames.ue4.SplashActivity
