5e93a567d9d3850680961d20ee1b8bb6d702a735
trankhanhtinh1 <ghettaodi22342@gmail.com>
2025-03-27 02:34:11 +0700

    update bypass

--- GL.bat
+++ GL.bat
@@ -3,5 +3,6 @@ adb start-server
 adb push libinvinsible.so /data/data/com.tencent.ig/lib/libinvinsible.so
 adb push libanortTW.so /data/data/com.tencent.ig/lib/libanort.so
 adb push libCrashSight.so /data/data/com.tencent.ig/lib/libCrashSight.so
+adb push libGVoicePlugin.so /data/data/com.tencent.ig/lib/libGVoicePlugin.so
 adb.exe -s emulator-5554 shell rm -rf /data/data/com.tencent.ig/databases
 adb.exe -s emulator-5554 shell am start -n com.tencent.ig/com.epicgames.ue4.SplashActivity
