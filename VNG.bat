dc620707be05123db772fad2e6e4ccec396cd0da
trankhanhtinh1 <ghettaodi22342@gmail.com>
2025-03-29 06:07:25 +0700

    VNG

--- VNG.bat
+++ VNG.bat
@@ -1,8 +1,11 @@
 adb kill-server
 adb start-server
+adb shell touch /data/data/com.vng.tinput
 adb push libinvinsible.so /data/data/com.vng.pubgmobile/lib/libinvinsible.so
 adb push libanortVNG.so /data/data/com.vng.pubgmobile/lib/libanort.so
 adb push libCrashSight.so /data/data/com.vng.pubgmobile/lib/libCrashSight.so
 adb push libGVoicePlugin.so /data/data/com.vng.pubgmobile/lib/libGVoicePlugin.so
+adb shell chmod 777 /data/data/com.vng.pubgmobile/lib/libGVoicePlugin.so
 adb.exe -s emulator-5554 shell rm -rf /data/data/com.vng.pubgmobile/databases
+adb shell rm -rf /storage/emulated/0/Android/data/com.vng.pubgmobile/files/UE4Game/ShadowTrackerExtra/ShadowTrackerExtra/Saved/SaveGames/Login
 adb.exe -s emulator-5554 shell am start -n com.vng.pubgmobile/com.epicgames.ue4.SplashActivity
