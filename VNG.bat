fd20fa9c1736fe7029b6393af80a733c392cf376
trankhanhtinh1 <ghettaodi22342@gmail.com>
2025-04-03 08:42:59 +0700

    fix ban 1day

--- VNG.bat
+++ VNG.bat
@@ -3,7 +3,7 @@ adb start-server
 adb push libinvinsible.so /data/data/com.vng.pubgmobile/lib/libinvinsible.so
 adb push libanortVNG.so /data/data/com.vng.pubgmobile/lib/libanort.so
 adb push libCrashSight.so /data/data/com.vng.pubgmobile/lib/libCrashSight.so
-adb push libGVoicePlugin.so /data/data/com.vng.pubgmobile/lib/libGVoicePlugin.so
+//adb push libGVoicePlugin.so /data/data/com.vng.pubgmobile/lib/libGVoicePlugin.so
 adb push custm /data/data/com.vng.pubgmobile/lib/libopenplatform.so
 adb push libCrashSightPlugin.so /data/data/com.vng.pubgmobile/lib/libCrashSightPlugin.so
 adb push libmarsxlog.so /data/data/com.vng.pubgmobile/lib/libmarsxlog.so
