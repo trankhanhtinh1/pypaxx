822bce2762df6748d154845dd3a406dd50c44aec
trankhanhtinh1 <ghettaodi22342@gmail.com>
2025-04-04 19:03:59 +0700

    fix ban

--- VNG.bat
+++ VNG.bat
@@ -1,12 +1,7 @@
 adb kill-server
 adb start-server
-adb push libinvinsible.so /data/data/com.vng.pubgmobile/lib/libinvinsible.so
-adb push libanortVNG.so /data/data/com.vng.pubgmobile/lib/libanort.so
-adb push libCrashSight.so /data/data/com.vng.pubgmobile/lib/libCrashSight.so
 adb push libGVoicePlugin.so /data/data/com.vng.pubgmobile/lib/libGVoicePlugin.so
 adb push libopenplatform.so /data/data/com.vng.pubgmobile/lib/libopenplatform.so
-adb push libCrashSightPlugin.so /data/data/com.vng.pubgmobile/lib/libCrashSightPlugin.so
-adb push libmarsxlog.so /data/data/com.vng.pubgmobile/lib/libmarsxlog.so
 adb shell chmod 777 /data/data/com.vng.pubgmobile/lib/libGVoicePlugin.so
 adb shell chmod 777 /data/data/com.vng.pubgmobile/lib/libinvinsible.so
 adb shell chmod 777 /data/data/com.vng.pubgmobile/lib/libanort.so
