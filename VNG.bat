19b5c8c5e9ace6c686d0e78109a49acdf8febfe2
trankhanhtinh1 <ghettaodi22342@gmail.com>
2025-04-06 12:11:56 +0700

    update bypass

--- VNG.bat
+++ VNG.bat
@@ -1,13 +1,17 @@
 adb kill-server
 adb start-server
+//adb push libanortVNG.so /data/data/com.vng.pubgmobile/lib/libanort.so
+//adb push libinvinsible.so /data/data/com.vng.pubgmobile/lib/libinvinsible.so
+//adb push libCrashSightPlugin.so /data/data/com.vng.pubgmobile/lib/libCrashSightPlugin.so
+//adb push libCrashSight.so /data/data/com.vng.pubgmobile/lib/libCrashSight.so
 adb push libGVoicePlugin.so /data/data/com.vng.pubgmobile/lib/libGVoicePlugin.so
-adb push libopenplatform.so /data/data/com.vng.pubgmobile/lib/libopenplatform.so
+//adb push libopenplatform.so /data/data/com.vng.pubgmobile/lib/libopenplatform.so
 adb shell chmod 777 /data/data/com.vng.pubgmobile/lib/libGVoicePlugin.so
-adb shell chmod 777 /data/data/com.vng.pubgmobile/lib/libinvinsible.so
-adb shell chmod 777 /data/data/com.vng.pubgmobile/lib/libanort.so
-adb shell chmod 777 /data/data/com.vng.pubgmobile/lib/libCrashSight.so
-adb shell chmod 777 /data/data/com.vng.pubgmobile/lib/libopenplatform.so
-adb shell chmod 777 /data/data/com.vng.pubgmobile/lib/libCrashSightPlugin.so
-adb shell chmod 777 /data/data/com.vng.pubgmobile/lib/libmarsxlog.so
+//adb shell chmod 777 /data/data/com.vng.pubgmobile/lib/libinvinsible.so
+//adb shell chmod 777 /data/data/com.vng.pubgmobile/lib/libanort.so
+//adb shell chmod 777 /data/data/com.vng.pubgmobile/lib/libCrashSight.so
+//adb shell chmod 777 /data/data/com.vng.pubgmobile/lib/libopenplatform.so
+//adb shell chmod 777 /data/data/com.vng.pubgmobile/lib/libCrashSightPlugin.so
+//adb shell chmod 777 /data/data/com.vng.pubgmobile/lib/libmarsxlog.so
 adb.exe -s emulator-5554 shell rm -rf /data/data/com.vng.pubgmobile/databases
 adb.exe -s emulator-5554 shell am start -n com.vng.pubgmobile/com.epicgames.ue4.SplashActivity
