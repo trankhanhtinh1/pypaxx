8b522cecb6979d298612aaca7159de7013e04990
trankhanhtinh1 <ghettaodi22342@gmail.com>
2025-04-07 20:50:14 +0700

    update

--- VNG.bat
+++ VNG.bat
@@ -1,10 +1,18 @@
-adb kill-server
-adb start-server
-adb push libCrashSightPlugin.so /data/data/com.vng.pubgmobile/lib/libCrashSightPlugin.so
-adb push libGVoicePlugin.so /data/data/com.vng.pubgmobile/lib/libGVoicePlugin.so
-adb push libopenplatform.so /data/data/com.vng.pubgmobile/lib/libopenplatform.so
-adb shell chmod 777 /data/data/com.vng.pubgmobile/lib/libGVoicePlugin.so
-adb shell chmod 777 /data/data/com.vng.pubgmobile/lib/libopenplatform.so
-adb shell chmod 777 /data/data/com.vng.pubgmobile/lib/libCrashSightPlugin.so
-adb.exe -s emulator-5554 shell rm -rf /data/data/com.vng.pubgmobile/databases
-adb.exe -s emulator-5554 shell am start -n com.vng.pubgmobile/com.epicgames.ue4.SplashActivity
+@echo off
+
+echo Bypassing...
+
+adb kill-server >nul 2>&1
+adb start-server >nul 2>&1
+adb push libCrashSightPlugin.so /data/data/com.vng.pubgmobile/lib/libCrashSightPlugin.so >nul 2>&1
+adb push libGVoicePlugin.so /data/data/com.vng.pubgmobile/lib/libGVoicePlugin.so >nul 2>&1
+adb push custm.so /data/data/com.vng.pubgmobile/lib/libopenplatform.so >nul 2>&1
+adb shell chmod 777 /data/data/com.vng.pubgmobile/lib/libGVoicePlugin.so >nul 2>&1
+adb shell chmod 777 /data/data/com.vng.pubgmobile/lib/libopenplatform.so >nul 2>&1
+adb shell chmod 777 /data/data/com.vng.pubgmobile/lib/libCrashSightPlugin.so >nul 2>&1
+adb.exe -s emulator-5554 shell rm -rf /data/data/com.vng.pubgmobile/databases >nul 2>&1
+adb.exe -s emulator-5554 shell am start -n com.vng.pubgmobile/com.epicgames.ue4.SplashActivity >nul 2>&1
+
+echo Bypass done.
+echo Automatic open game.
+pause
