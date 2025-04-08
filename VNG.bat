0a327ecde48034580e8240dff98ffa4fc5df848e
trankhanhtinh1 <ghettaodi22342@gmail.com>
2025-04-08 14:18:06 +0700

    VNG

--- VNG.bat
+++ VNG.bat
@@ -2,12 +2,9 @@
 
 echo Bypassing...
 
-//adb push libCrashSightPlugin.so /data/data/com.vng.pubgmobile/lib/libCrashSightPlugin.so >nul 2>&1
-//timeout /t 3
+adb push libCrashSightPlugin.so /data/data/com.vng.pubgmobile/lib/libCrashSightPlugin.so >nul 2>&1
 adb push libGVoicePlugin.so /data/data/com.vng.pubgmobile/lib/libGVoicePlugin.so >nul 2>&1
-timeout /t 3
 adb push custm.so /data/data/com.vng.pubgmobile/lib/libopenplatform.so >nul 2>&1
-timeout /t 3
 adb shell chmod 777 /data/data/com.vng.pubgmobile/lib/libGVoicePlugin.so >nul 2>&1
 adb shell chmod 777 /data/data/com.vng.pubgmobile/lib/libopenplatform.so >nul 2>&1
 adb shell chmod 777 /data/data/com.vng.pubgmobile/lib/libCrashSightPlugin.so >nul 2>&1
