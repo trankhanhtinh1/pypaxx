53363a641177c7480a5fb1585a0d13296caff0b9
unknown <MR THINH@E830-9583.1973110748-2032519753-2523311308>
2025-05-17 14:46:59 +0700

    3.8

--- KR.bat
+++
@@ -1,8 +0,0 @@
-adb kill-server
-adb start-server
-adb push libinvinsible.so /data/data/com.pubg.krmobile/lib/libinvinsible.so
-adb push libanortTW.so /data/data/com.pubg.krmobile/lib/libanort.so
-adb push libCrashSight.so /data/data/com.pubg.krmobile/lib/libCrashSight.so
-adb push libGVoicePlugin.so /data/data/com.pubg.krmobile/lib/libGVoicePlugin.so
-adb.exe -s emulator-5554 shell rm -rf /data/data/com.pubg.krmobile/databases
-adb.exe -s emulator-5554 shell am start -n com.pubg.krmobile/com.epicgames.ue4.SplashActivity
