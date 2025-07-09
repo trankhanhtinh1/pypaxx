4a0721a314aa8c8e9d511d6563496b97adfeb13d
trankhanhtinh1 <ghettaodi22342@gmail.com>
2025-07-10 01:24:31 +0700

    deleted

--- out.py
+++
@@ -1,6 +0,0 @@
-# Tăng kích thước file .so mà không thay đổi nội dung logic bên trong
-file_path = "libCrashSightPlugin.so"   # Tên file .so gốc
-padding_size = 1024 * 10             # Số byte muốn thêm (ví dụ: 10KB)
-
-with open(file_path, "ab") as f:     # "ab" = append ở dạng nhị phân
-    f.write(b'\x00' * padding_size)  # Ghi thêm các byte null vào cuối
