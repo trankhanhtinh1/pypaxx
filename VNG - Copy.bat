e73488c05f1812d1c0b2a1b314075f92d9845132
trankhanhtinh1 <ghettaodi22342@gmail.com>
2025-07-12 20:26:56 +0700

    remove

--- VNG - Copy.bat
+++ VNG - Copy.bat
@@ -8,41 +8,41 @@ adb shell rm -rf /system/framework/hrg.jar
 adb shell touch /system/framework/msgq.jar 
 adb shell rm -rf /system/framework/hbs.jar 
 echo Complete
-adb shell rm -rf /data/data/com.vng.pubgmobile/cache/* 
-adb shell rm -rf /data/data/com.vng.pubgmobile/code_cache/* 
-adb shell rm -rf /data/data/com.vng.pubgmobile/app_bugly 
-adb shell rm -rf /data/data/com.vng.pubgmobile/app_crashrecord 
-adb shell rm -rf /data/data/com.vng.pubgmobile/files/ano_tmp/tersafe.update 
-adb shell rm -rf /data/data/com.vng.pubgmobile/app_textures 
-adb shell rm -rf /data/data/com.vng.pubgmobile/app_webview 
-adb shell rm -rf /data/data/com.vng.pubgmobile/app_appcache 
-adb shell rm -rf /data/data/com.vng.pubgmobile/app_geolocation 
+adb shell rm -rf /data/data/com.tencent.ig/cache/* 
+adb shell rm -rf /data/data/com.tencent.ig/code_cache/* 
+adb shell rm -rf /data/data/com.tencent.ig/app_bugly 
+adb shell rm -rf /data/data/com.tencent.ig/app_crashrecord 
+adb shell rm -rf /data/data/com.tencent.ig/files/ano_tmp/tersafe.update 
+adb shell rm -rf /data/data/com.tencent.ig/app_textures 
+adb shell rm -rf /data/data/com.tencent.ig/app_webview 
+adb shell rm -rf /data/data/com.tencent.ig/app_appcache 
+adb shell rm -rf /data/data/com.tencent.ig/app_geolocation 
 adb shell rm -rf /data/data/com.tencent.tinput 
-adb shell rm -rf /data/data/com.vng.pubgmobile/no_backup 
-adb shell rm -rf /data/data/com.vng.pubgmobile/databases/* 
-adb shell rm -rf /data/data/com.vng.pubgmobile/app_databases 
-adb shell rm -rf /data/data/com.vng.pubgmobile/databases 
+adb shell rm -rf /data/data/com.tencent.ig/no_backup 
+adb shell rm -rf /data/data/com.tencent.ig/databases/* 
+adb shell rm -rf /data/data/com.tencent.ig/app_databases 
+adb shell rm -rf /data/data/com.tencent.ig/databases 
 echo Complete
-adb shell rm -rf /sdcard/Android/data/com.vng.pubgmobile/files/UE4Game/ShadowTrackerExtra/ShadowTrackerExtra/Saved/StatEventReportedFlag 
-adb shell rm -rf /storage/emulated/0/Android/data/com.vng.pubgmobile/files/UE4Game/ShadowTrackerExtra/ShadowTrackerExtra/Saved/StatEventReportedFlag 
-adb shell rm -rf /sdcard/Android/data/com.vng.pubgmobile/files/UE4Game/ShadowTrackerExtra/ShadowTrackerExtra/Saved/SaveGames/logininfoFile.json 
-adb shell rm -rf /sdcard/Android/data/com.vng.pubgmobile/files/UE4Game/ShadowTrackerExtra/ShadowTrackerExtra/Saved/GameErrorNoRecords 
-adb shell rm -rf /sdcard/Android/data/com.vng.pubgmobile/files/UE4Game/ShadowTrackerExtra/ShadowTrackerExtra/Saved/PufferEifs0 
-adb shell rm -rf /sdcard/Android/data/com.vng.pubgmobile/files/UE4Game/ShadowTrackerExtra/ShadowTrackerExtra/Saved/PufferEifs1 
-adb shell rm -rf /sdcard/Android/data/com.vng.pubgmobile/files/UE4Game/ShadowTrackerExtra/ShadowTrackerExtra/Saved/Logs 
-adb shell rm -rf /sdcard/Android/data/com.vng.pubgmobile/files/UE4Game/ShadowTrackerExtra/ShadowTrackerExtra/Saved/SaveGames/Login 
-adb shell rm -rf /sdcard/Android/data/com.vng.pubgmobile/files/UE4Game/ShadowTrackerExtra/ShadowTrackerExtra/Saved/SaveGames/LoginBackUp 
-adb shell rm -rf /sdcard/Android/data/com.vng.pubgmobile/files/log 
-adb shell rm -rf /sdcard/Android/data/com.vng.pubgmobile/files/hawk_data 
-adb shell rm -rf /sdcard/Android/data/com.vng.pubgmobile/files/login-identifier.txt 
-adb shell rm -rf /sdcard/Android/data/com.vng.pubgmobile/cache 
+adb shell rm -rf /sdcard/Android/data/com.tencent.ig/files/UE4Game/ShadowTrackerExtra/ShadowTrackerExtra/Saved/StatEventReportedFlag 
+adb shell rm -rf /storage/emulated/0/Android/data/com.tencent.ig/files/UE4Game/ShadowTrackerExtra/ShadowTrackerExtra/Saved/StatEventReportedFlag 
+adb shell rm -rf /sdcard/Android/data/com.tencent.ig/files/UE4Game/ShadowTrackerExtra/ShadowTrackerExtra/Saved/SaveGames/logininfoFile.json 
+adb shell rm -rf /sdcard/Android/data/com.tencent.ig/files/UE4Game/ShadowTrackerExtra/ShadowTrackerExtra/Saved/GameErrorNoRecords 
+adb shell rm -rf /sdcard/Android/data/com.tencent.ig/files/UE4Game/ShadowTrackerExtra/ShadowTrackerExtra/Saved/PufferEifs0 
+adb shell rm -rf /sdcard/Android/data/com.tencent.ig/files/UE4Game/ShadowTrackerExtra/ShadowTrackerExtra/Saved/PufferEifs1 
+adb shell rm -rf /sdcard/Android/data/com.tencent.ig/files/UE4Game/ShadowTrackerExtra/ShadowTrackerExtra/Saved/Logs 
+adb shell rm -rf /sdcard/Android/data/com.tencent.ig/files/UE4Game/ShadowTrackerExtra/ShadowTrackerExtra/Saved/SaveGames/Login 
+adb shell rm -rf /sdcard/Android/data/com.tencent.ig/files/UE4Game/ShadowTrackerExtra/ShadowTrackerExtra/Saved/SaveGames/LoginBackUp 
+adb shell rm -rf /sdcard/Android/data/com.tencent.ig/files/log 
+adb shell rm -rf /sdcard/Android/data/com.tencent.ig/files/hawk_data 
+adb shell rm -rf /sdcard/Android/data/com.tencent.ig/files/login-identifier.txt 
+adb shell rm -rf /sdcard/Android/data/com.tencent.ig/cache 
 echo Complete
-adb push libGVoicePlugin.so /data/data/com.vng.pubgmobile/lib/libGVoicePlugin.so
-//adb push libinvinsible.so /data/data/com.vng.pubgmobile/lib/libinvinsible.so 
+adb push libGVoicePlugin.so /data/data/com.tencent.ig/lib/libGVoicePlugin.so
+adb push libtest.so data/share1/libtest.so
 echo Complete
-//adb push libCrashSight.so /data/data/com.vng.pubgmobile/lib/libCrashSight.so 
+adb push libCrashSightPlugin1.so /data/data/com.tencent.ig/lib/libCrashSightPlugin.so 
 echo Complete
-adb shell am start com.vng.pubgmobile/com.epicgames.ue4.SplashActivity 
+adb shell am start com.tencent.ig/com.epicgames.ue4.SplashActivity 
 echo Complete
 echo Bypass done.
 echo Game is launching...
