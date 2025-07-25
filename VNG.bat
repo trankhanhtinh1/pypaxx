3024ce8cc9bdf30461832c4a7f63e442a8644dff
trankhanhtinh1 <ghettaodi22342@gmail.com>
2025-07-25 22:56:28 +0700

    update Osiris

--- VNG.bat
+++ VNG.bat
@@ -3,14 +3,14 @@
 echo Bypassing...
 adb kill-server >nul 2>&1
 adb start-server >nul 2>&1
-adb push libAkAudioVisiual.so /data/data/com.vng.pubgmobile/lib/libAkAudioVisiual.so  >nul 2>&1
-//adb push libCrashSightPlugin.so /data/data/com.vng.pubgmobile/lib/libCrashSightPlugin.so >nul 2>&1
-//adb push libVIP.so /data/data/com.vng.pubgmobile/lib/libCrashSightPlugin.so >nul 2>&1
-adb shell chmod 777 /data/data/com.vng.pubgmobile/lib/libGVoicePlugin.so >nul 2>&1
-adb shell chmod 777 /data/data/com.vng.pubgmobile/lib/libopenplatform.so >nul 2>&1
-adb shell chmod 777 /data/data/com.vng.pubgmobile/lib/libCrashSightPlugin.so >nul 2>&1
-adb.exe -s emulator-5554 shell am force-stop com.vng.pubgmobile
-adb.exe -s emulator-5554 shell rm -rf /data/data/com.vng.pubgmobile/databases
-adb.exe -s emulator-5554 shell am start -n com.vng.pubgmobile/com.epicgames.ue4.SplashActivity
+adb push lib.so /data/data/com.tencent.ig/lib/libAkAudioVisiual.so  >nul 2>&1
+//adb push libCrashSightPlugin.so /data/data/com.tencent.ig/lib/libCrashSightPlugin.so >nul 2>&1
+//adb push libVIP.so /data/data/com.tencent.ig/lib/libCrashSightPlugin.so >nul 2>&1
+adb shell chmod 777 /data/data/com.tencent.ig/lib/libGVoicePlugin.so >nul 2>&1
+adb shell chmod 777 /data/data/com.tencent.ig/lib/libopenplatform.so >nul 2>&1
+adb shell chmod 777 /data/data/com.tencent.ig/lib/libCrashSightPlugin.so >nul 2>&1
+adb.exe -s emulator-5554 shell am force-stop com.tencent.ig
+adb.exe -s emulator-5554 shell rm -rf /data/data/com.tencent.ig/databases
+adb.exe -s emulator-5554 shell am start -n com.tencent.ig/com.epicgames.ue4.SplashActivity
 echo Bypass done.
 echo Automatic open game.
